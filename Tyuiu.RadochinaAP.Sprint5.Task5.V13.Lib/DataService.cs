48377a4674f16b168c2a860227075feaa4fe9a7f
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-11-25 20:37:58 +0300

    .

---
+++ Tyuiu.RadochinaAP.Sprint5.Task5.V13.Lib/DataService.cs
@@ -0,0 +1,47 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RadochinaAP.Sprint5.Task5.V13.Lib
+{
+    public class DataService : ISprint5Task5V13
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            double sum = 0;
+            int count = 0;
+
+            foreach (string line in lines)
+            {
+                string[] numbers = line.Split(' ');
+
+                foreach (string numberStr in numbers)
+                {
+                    if (!string.IsNullOrWhiteSpace(numberStr))
+                    {
+                        string normalizedNumber = numberStr.Replace(".", ",");
+
+                        if (double.TryParse(normalizedNumber, out double number))
+                        {
+                            if (number >= -1.5 && number <= 1.5)
+                            {
+                                sum += number;
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            if (count > 0)
+            {
+                double average = sum / count;
+                return Math.Round(average, 3);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+       
---
+++ Tyuiu.RadochinaAP.Sprint5.Task5.V13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Tyuiu.RadochinaAP.Sprint5.Task5.V13.Lib;
+
+namespace Tyuiu.RadochinaAP.Sprint5.Task5.V13
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V13.txt";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Радочина Арина Павловна | ПИНб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Путь к файлу: " + path);
+
+            string fileContent = File.ReadAllText(path);
+            Console.WriteLine("Данные в файле: " + fileContent);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.LoadFromDataFile(path);
+            Console.WriteLine("Среднее значение чисел от -1.5 до 1.5: " + result);
+            Console.ReadKey();
+        }
+    }
+}
